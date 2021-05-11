306bbe03e25a3d9be96105a4aa3d5ddb4f61e368
Pavel Emelyanov <xemul@scylladb.com>
2021-05-11 20:47:45 +0300

    io_queue: Move request weighting code into group
    The logic that calculates the ticket for a given requests depends
    purely on queue's config which now lives on the group, so it's
    natural to make it a group's method.
    
    Also, while at it, split the ticket generation method into two,
    since its first half is going to complicate soon and the mixed
    mode patches will need the 2nd half as well.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- include/seastar/core/io_queue.hh
+++ include/seastar/core/io_queue.hh
@@ -162,6 +162,8 @@ class io_group {
 public:
     explicit io_group(io_queue::config io_cfg) noexcept;
 
+    fair_queue_ticket request_fq_ticket(const internal::io_request& req, size_t len) const;
+
 private:
     friend class io_queue;
     fair_group _fg;
@@ -170,6 +172,7 @@ private:
     const io_queue::config& get_config() const noexcept { return _config; }
 
     static fair_group::config make_fair_group_config(io_queue::config qcfg) noexcept;
+    fair_queue_ticket make_ticket(unsigned weight, size_t sz, size_t len) const noexcept;
 };
 
 inline const io_queue::config& io_queue::get_config() const noexcept {

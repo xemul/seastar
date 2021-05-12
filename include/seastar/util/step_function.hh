2444494addfc7938f946221e253c0b9280a514d0
Pavel Emelyanov <xemul@scylladb.com>
2021-05-12 10:30:39 +0300

    utils(internal): Step-function implementation
    Pretty much all the disks that were tested showed different bandwidth
    values depending on used IO parameters, e.g. when using requests of
    different lengths or mixing reads with writes. In most of the learned
    cases the observed bandwidths could be described with good accuracy by
    a step function. IOW -- when IO parameters change the measured
    bandwidth values changed "in steps" when the parameters crossed some
    threshold.
    
    So here's some simple implementation of a function that looks like
    y = F(x) = { F_i, when x <= x_i } or F_default otherwise.
    
    The implementation is O(n) where n is the number of steps since the disk
    stats collected could always be described with at most 3 steps.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

---
+++ include/seastar/util/step_function.hh
@@ -0,0 +1,95 @@
+/*
+ * This file is open source software, licensed to you under the terms
+ * of the Apache License, Version 2.0 (the "License").  See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership.  You may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+/*
+ * Copyright 2021-present ScyllaDB
+ */
+
+#pragma once
+
+#include <boost/container/small_vector.hpp>
+#include <type_traits>
+#include <seastar/util/concepts.hh>
+
+namespace seastar {
+namespace internal {
+
+/// Left-continuous staricase function implementation
+/// The function is y = F(x) = { F_i if x <= x_i } or F_default
+template <typename T, unsigned Inline = 2, typename Index = size_t>
+SEASTAR_CONCEPT( requires std::is_arithmetic_v<T> && std::is_arithmetic_v<Index> )
+class step_function {
+    boost::container::small_vector<std::pair<Index, T>, Inline> _steps;
+    template <typename U, unsigned V>
+    friend std::ostream& operator<<(std::ostream&, const step_function<U, V>&);
+
+public:
+    /// Constructs the step_function with \c def as the default value
+    step_function(T def) noexcept {
+        _steps.emplace_back(std::numeric_limits<Index>::max(), def);
+    }
+
+    /// Returns the value corresponding to \c x
+    T operator()(Index x) const noexcept {
+        for (auto&& s : _steps) {
+            if (x <= s.first) {
+                return s.second;
+            }
+        }
+
+        __builtin_unreachable(); // cannot get here, the last element is always max
+    }
+
+    /// Adds a step at length \c x with the value \c y
+    void add_step(Index x, T y) {
+        if (x == std::numeric_limits<Index>::max()) {
+            throw std::overflow_error("Cannot add step at the end of the x-range");
+        }
+
+        auto pos = _steps.begin();
+        while (pos->first < x) {
+            pos++; // will hit the last max element in the worst case
+        }
+        _steps.emplace(pos, x, y);
+    }
+
+    /// Changes the default value to \c y
+    void set_default(T y) noexcept {
+        _steps.back().second = y;
+    }
+
+    T get_default() const noexcept {
+        return _steps.back().second;
+    }
+};
+
+template <typename T, unsigned Inline>
+inline std::ostream& operator<<(std::ostream& out, const step_function<T, Inline>& st) {
+    out << "step_function[";
+    for (auto&& s : st._steps) {
+        if (s == st._steps.back()) {
+            out << " *:" << s.second;
+        } else {
+            out << " " << s.first << ":" << s.second;
+        }
+    }
+    out << " ]";
+    return out;
+}
+
+} // namespace internal
+} // namespace seastar
---
+++ tests/unit/step_function_test.cc
@@ -0,0 +1,73 @@
+/*
+ * This file is open source software, licensed to you under the terms
+ * of the Apache License, Version 2.0 (the "License").  See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership.  You may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+/*
+ * Copyright 2021-present ScyllaDB
+ */
+
+#include <seastar/testing/test_case.hh>
+#include <seastar/testing/thread_test_case.hh>
+#include <seastar/core/do_with.hh>
+#include <seastar/util/step_function.hh>
+#include <fmt/core.h>
+
+using namespace seastar;
+
+SEASTAR_TEST_CASE(step_function_test) {
+    constexpr size_t min = std::numeric_limits<size_t>::min();
+    constexpr size_t max = std::numeric_limits<size_t>::max();
+
+    internal::step_function<int, 5> stairs(1);
+    auto check_interval = [&stairs] (size_t from, size_t to, int val) {
+        BOOST_REQUIRE(stairs(from) == val);
+        BOOST_REQUIRE(stairs(to) == val);
+    };
+
+    check_interval(min, max, 1);
+
+    stairs.add_step(100, 2);
+    check_interval(min, 100, 2);
+    check_interval(101, max, 1);
+
+    stairs.add_step(200, 3);
+    check_interval(min, 100, 2);
+    check_interval(101, 200, 3);
+    check_interval(201, max, 1);
+
+    stairs.add_step(150, 4);
+    check_interval(min, 100, 2);
+    check_interval(101, 150, 4);
+    check_interval(151, 200, 3);
+    check_interval(201, max, 1);
+
+    stairs.add_step(50, 5);
+    check_interval(min, 50, 5);
+    check_interval(51, 100, 2);
+    check_interval(101, 150, 4);
+    check_interval(151, 200, 3);
+    check_interval(201, max, 1);
+
+    stairs.add_step(250, 6);
+    check_interval(min, 50, 5);
+    check_interval(51, 100, 2);
+    check_interval(101, 150, 4);
+    check_interval(151, 200, 3);
+    check_interval(201, 250, 6);
+    check_interval(251, max, 1);
+
+    return make_ready_future<>();
+}

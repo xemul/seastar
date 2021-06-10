ac155b39dfc659b6767890218adf3419dd537732
Pavel Emelyanov <xemul@scylladb.com>
2021-06-10 13:08:11 +0300

    io_queue: Consider mixed limitations
    When advertising the maximum request sizes also cap them with
    the mixed-mode maximums. These might not work for the pure modes,
    but there's no way to dynamically adjust it right now, so make
    this limitation static.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- include/seastar/core/io_queue.hh
+++ include/seastar/core/io_queue.hh
@@ -141,6 +141,8 @@ public:
         float disk_us_per_byte = 0;
         size_t disk_read_saturation_length = std::numeric_limits<size_t>::max();
         size_t disk_write_saturation_length = std::numeric_limits<size_t>::max();
+        size_t disk_mixed_read_max_length = std::numeric_limits<size_t>::max();
+        size_t disk_mixed_write_max_length = std::numeric_limits<size_t>::max();
         internal::step_function<unsigned, 2> disk_bytes_mixed_read_multiplier = internal::step_function<unsigned, 2>(read_request_base_count);
         sstring mountpoint = "undefined";
     };
